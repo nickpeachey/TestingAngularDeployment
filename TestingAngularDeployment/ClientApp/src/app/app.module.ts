68c5515a608bf387a068150ab53f938ef780ae7f
Nick Peachey <nick.peachey@gmail.com>
2018-07-17 14:59:31 +0100

    removal
    removed random view

--- TestingAngularDeployment/ClientApp/src/app/app.module.ts
+++ TestingAngularDeployment/ClientApp/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { RandomViewComponent } from './random-view/random-view/random-view.component'
 
 @NgModule({
   declarations: [
@@ -17,8 +16,7 @@ import { RandomViewComponent } from './random-view/random-view/random-view.compo
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent,
-    RandomViewComponent
+    FetchDataComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -27,8 +25,7 @@ import { RandomViewComponent } from './random-view/random-view/random-view.compo
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'random-view', component: RandomViewComponent }
+      { path: 'fetch-data', component: FetchDataComponent }
     ])
   ],
   providers: [],
