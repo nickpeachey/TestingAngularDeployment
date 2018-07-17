790f2b66bb66a56cabd7f23549df659027d64602
Nick Peachey <nick.peachey@gmail.com>
2018-07-17 14:52:00 +0100

    added random view component to test changes flowing throw into deployment
    added random view component to test changes flowing throw into deployment

---
+++ TestingAngularDeployment/ClientApp/src/app/random-view/random-view/random-view.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-random-view',
+    templateUrl: './random-view.component.html',
+    styleUrls: ['./random-view.component.css']
+})
+/** random-view component*/
+export class RandomViewComponent {
+    /** random-view ctor */
+    constructor() {
+
+    }
+}
